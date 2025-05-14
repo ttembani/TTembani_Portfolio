effe8aa4a8d0b768e05154e0d40c45ff514e7fb7
ttembani <tembanithulisa2@gmail.com>
2025-05-14 08:38:04 +0200

    Portfolio

--- T.Tembani_Portfolio/main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(" .text" , {
-    strings:["Frontend Developer", "App Developer" , "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
